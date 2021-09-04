892685f412245d7ea378f6386f658436afa041b9
Matheus <matheus@pop-os.localdomain>
2021-09-03 21:04:22 -0300

    feat: added function to get the current url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-undef */
+import { useEffect, useState } from "react/cjs/react.production.min";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = tabs[0].url;
+      setUrl(url);
+    });
+  }, []);
+
   return (
     <Dropdown className="App">
-      <h1>Hello chrome extension</h1>
+      <h1>{url}</h1>
     </Dropdown>
   );
 }
